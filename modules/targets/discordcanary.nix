1b0607920261342ef09c201c5a4d872a929a9a05
Rafael Grossi <108767275+xendak@users.noreply.github.com>
2026-06-25 13:03:06 -0300

    fix

--- modules/targets/discordcanary.nix
+++ modules/targets/discordcanary.nix
@@ -3,7 +3,7 @@
   options.themes.targets-discordcanary.enable = lib.mkEnableOption "discordcanary theme target";
 
   config = lib.mkIf (config.themes.enable && config.themes.targets-discordcanary.enable) {
-    themes.targets."discord/settings.json_clone1" =
+    themes.targets."discord/settings.json_clone_1" =
       "${config.home.homeDirectory}/.config/discordcanary/settings.json";
   };
 }
